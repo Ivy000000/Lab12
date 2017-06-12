5f256b9f253017ce73f8bc928c8ce27214b693dc
Ivy000000 <ivy9297@gmail.com>
2017-06-12 20:18:39 +0800

    使用手機搜尋是否找得到 ESP-8266 之基地台

---
+++ lab12.ino
@@ -0,0 +1,21 @@
+#include <ESP8266.h>
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial sSerial(11,10); //(RX,TX)
+
+void setup() {
+  sSerial.begin(115200);  
+  Serial.begin(115200);    
+  Serial.println("SoftSerial to ESP8266 AT commands test ...");
+  }
+
+void loop() {
+  if (sSerial.available()) {  
+    Serial.write(sSerial.read());  
+  
+    }
+  if (Serial.available()) {  
+    sSerial.write(Serial.read()); 
+    }
+  }
